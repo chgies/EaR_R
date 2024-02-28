3e9a3bcc8311981db92c990b3a7e43c97675501f
Christoph Giesecke <132996159+chgies@users.noreply.github.com>
2024-02-28 17:06:31 +0100

    Reworked file structure, added function description

--- caer_processing/models/CAER_model_training.py
+++ caer_processing/models/CAER_model_training.py
@@ -1,11 +1,25 @@
 import pandas as pd
 import numpy as np
+import os
 import torch
 from torch import nn
 from torch.utils.data import DataLoader,TensorDataset
+import EmotionV0
 
-def prepare_training():
-    NUM_CLASSES = 1
+if torch.cuda.is_available():
+    device = "cuda"
+else:
+    device = "cpu"
+
+def train_and_test_model():
+    """
+    Run the loop of preparing the training data, train the model and evaluate it.
+        Params:
+            None
+        Returns:
+            None
+    """
+    NUM_CLASSES = 7
     NUM_FEATURES = 57
     device = "cuda" if torch.cuda.is_available() else "cpu"
     train_file_path = "/###/CAER_CSV/extracted_train_values.csv"  # Replace with the actual path to your CSV file
@@ -24,44 +38,46 @@ def prepare_training():
     test_dataset = TensorDataset(X_test, y_test)
     train_dl = DataLoader(train_dataset, batch_size = 12800, shuffle=True)
     test_dl = DataLoader(test_dataset, batch_size = 12800, shuffle=True)
-
-if torch.cuda.is_available():
-    device = "cuda"
-else:
-    device = "cpu"
-
-class EmotionV0(nn.Module):
-    """
-    The first version of the Neural Net model that gets emotions out of Video data.
-    Still a test version!
-    """
-    def __init__(self, input_size, hidden_size, output_size):
-        super(EmotionV0, self).__init__()
-        self.fc1 = nn.Linear(input_size, hidden_size)
-        self.relu = nn.ReLU()
-        self.fc2 = nn.Linear(hidden_size, output_size)
-
-    def forward(self, x):
-        x = x.to(self.fc1.weight.device)
-        x = self.fc1(x)
-        x = self.relu(x)
-        x = self.fc2(x)
-        return x
+    num_of_epochs = 20
+    # Put data to target device
+    X_train, y_train = X_train.to(device), y_train.to(device)
+    X_test, y_test = X_test.to(device), y_test.to(device)
+    torch.manual_seed(42)
+    ### Training
+    train_model = EmotionV0(NUM_FEATURES,104,NUM_CLASSES).to(device)
+    train_model(train_model, NUM_FEATURES, 104, NUM_CLASSES, train_dl, num_of_epochs)
+    # After training the model using train_model function,
+    # Evaluate the model on the validation set
+    val_model = EmotionV0(NUM_FEATURES,104,NUM_CLASSES).to(device)
+    load_model_weights(val_model)
+    evaluate_model(val_model, test_dl)
 
 # Function to convert integer labels to one-hot encoded vectors
 def convert_to_one_hot(labels, num_classes):
     """
     Convert the "emotion" column into 7 columns for model training
+        Params:
+            labels (Integer): The emotion labels as Integer, from 1 to 7
+            num_classes: The amount of classes that the Model needs to find
+        Returns:
+            one_hot (Tensor of float): The labels splitted into a tensor of 7 x 7 values
     """
     one_hot = torch.nn.functional.one_hot(labels - 1, num_classes).float()
     return one_hot.view(-1, num_classes)
 
-def train_model(input_size, hidden_size, output_size, data_loader, num_epochs=100, learning_rate=0.001):
+def train_model(model, input_size, hidden_size, output_size, data_loader, num_epochs=100, learning_rate=0.001):
     """
-    Train the model
+    Train the model with the given arguments as parameters
+        Params:
+            model (torch.nn.Module): The model that should be trained
+            input_size (int): How much features the model has
+            hidden_size (int): The size of the hidden layers
+            output_size (int): The amount of labels (classes) that form the output
+            data_loader (torch.utils.data.DataLoader): The Training input data formated into Tensors with a maximum chunk size
+            num_epochs (int): The number of training cycles
+            learning_rate (float): The learning rate used to train the model
     """
     # Initialize the loss function, and optimizer
-    model = EmotionV0(input_size,hidden_size, output_size)
     criterion = nn.CrossEntropyLoss()
     optimizer = torch.optim.Adam(model.parameters(), lr=learning_rate)
 
@@ -83,45 +99,44 @@ def train_model(input_size, hidden_size, output_size, data_loader, num_epochs=10
         # Print loss for every epoch
         if (epoch + 1) % 1 == 0:
             print(f'Epoch [{epoch+1}/{num_epochs}], Loss: {loss.item():.4f}')
-
     print('Training complete!')
-    
-
-"""
-# Make predictions with the model
-untrained_preds = model_0(X_test.to(device))
-print(f"Length of predictions: {len(untrained_preds)}, Shape: {untrained_preds.shape}")
-print(f"Length of test samples: {len(y_test)}, Shape: {y_test.shape}")
-print(f"\nFirst 10 predictions:\n{untrained_preds[:10]}")
-print(f"\nFirst 10 test labels:\n{y_test[:10]}")
-"""
-
-# Fit the model
-torch.manual_seed(42)
-
-# Set number of epochs
-epochs = 20
-
-
-# Put data to target device
-#X_train, y_train = X_train.to(device), y_train.to(device)
-#X_test, y_test = X_test.to(device), y_test.to(device)
-
-### Training
-#train_model(57, 104, 7, train_dl, epochs)
 
 # Function to save the model weights
-def save_model_weights(model, filepath='./CAER_model_weights.pth'):
+def save_model_weights(model, filepath='CAER_model_weights.pth'):
+    """
+    Save the trained weights into a file
+        Params:
+            model (torch.nn.Module): An instance of the model that has been trained
+            filepath (String): The path where the weights are saved
+        Returns:
+            None
+    """
     torch.save(model.state_dict(), filepath)
     print(f'Model weights saved to {filepath}')
 
 # Function to load the model weights
-def load_model_weights(model, filepath='./CAER_model_weights.pth'):
+def load_model_weights(model, filepath='CAER_model_weights.pth'):
+    """
+    Load saved weights into the model
+        Params:
+            model (torch.nn.Module): An instance of the model that needs these weights
+            filepath (String): The path where the weights are saved
+    Returns:
+        None
+    """
     model.load_state_dict(torch.load(filepath))
     print(f'Model weights loaded from {filepath}')
 
 # Function to evaluate the model on a validation set
 def evaluate_model(model, val_loader):
+    """
+    Evaluate the recently trained model
+        Params:
+            model (torch.nn.Module): The model that is evaluated
+            val_loader(torch.utils.data.DataLoader): The validation input data, splitted into chunks and formatted into Tensor format
+        Returns:
+            None
+    """
     model.eval()  # Set the model to evaluation mode
     correct = 0
     total = 0
@@ -139,8 +154,4 @@ def evaluate_model(model, val_loader):
     accuracy = correct / total
     print(f'Accuracy on validation set: {accuracy * 100:.2f}%')
 
-# After training the model using train_model function,
-# Evaluate the model on the validation set
-val_model = EmotionV0(57,104,7).to(device)
-load_model_weights(val_model)
-evaluate_model(val_model, test_dl)
+train_and_test_model()
