e9db8eed1d44d8a2983e2f9f73f70b5004a86663
Christoph Giesecke <132996159+chgies@users.noreply.github.com>
2024-04-06 16:08:21 +0200

    changed model naming to match their function

--- caer_processing/models/emotionV50/EmotionV50.py
+++ caer_processing/models/emotionV50/EmotionV50.py
@@ -1,20 +1,20 @@
 from torch import nn, relu, flatten, mean, argmax
 
-import torch
-class EmotionV3(nn.Module):
+class EmotionV50(nn.Module):
     """
     The feature-reduced version of the Neural Net model that gets emotions out of Video data.
+    This Model includes all features that make up to 50% importance, measured with Random Forest Classifier (see CAER_model_training.py)
     Still a test version!
     """
     def __init__(self, input_size, hidden_size, output_size):
-        super(EmotionV3, self).__init__()
+        super(EmotionV50, self).__init__()
         self.fc1 = nn.Linear(input_size, hidden_size)
         self.fc2 = nn.Linear(hidden_size, 64)
         self.fc3 = nn.Linear(64, output_size)
         self.test_dl = 0
         self.train_dl = 0
-        # Calculate mean and standard deviation of the dataset
-    def forward(self, x):        
+
+    def forward(self, x):
         x = x.to(self.fc1.weight.device)
         x = self.fc1(x)
         x = self.fc2(x)
--- caer_processing/models/emotionV80/EmotionV80.py
+++ caer_processing/models/emotionV80/EmotionV80.py
@@ -1,19 +1,20 @@
 from torch import nn, relu, flatten, mean, argmax
 
-class EmotionV2(nn.Module):
+class EmotionV80(nn.Module):
     """
     The feature-reduced version of the Neural Net model that gets emotions out of Video data.
+    This Model includes all features that make up to 80% importance, measured with Random Forest Classifier (see CAER_model_training.py)
     Still a test version!
     """
     def __init__(self, input_size, hidden_size, output_size):
-        super(EmotionV2, self).__init__()
+        super(EmotionV80, self).__init__()
         self.fc1 = nn.Linear(input_size, hidden_size)
         self.fc2 = nn.Linear(hidden_size, 64)
         self.fc3 = nn.Linear(64, output_size)
         self.test_dl = 0
         self.train_dl = 0
-
-    def forward(self, x):
+        # Calculate mean and standard deviation of the dataset
+    def forward(self, x):        
         x = x.to(self.fc1.weight.device)
         x = self.fc1(x)
         x = self.fc2(x)
