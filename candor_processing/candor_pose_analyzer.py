19dc088c7c2fdd864c5b3a00c194e65056b1cead
chgies <132996159+chgies@users.noreply.github.com>
2024-07-03 18:55:51 +0200

    Added more CMD options, reorganised file usage.

--- candor_processing/candor_pose_analyzer.py
+++ candor_processing/candor_pose_analyzer.py
@@ -11,9 +11,22 @@ from urllib.request import urlretrieve
 
 class CANDORPoseAnalyzer():
 
-    def __init__(self, landmark_type, dir_to_extract):
+    def __init__(self, landmark_type, dir_to_extract, show_fps, show_video):
+        """
+        The CANDORPoseAnalyzer class holds the methods to extract the pose coordinates out of the CANDOR dataset. It utilizes the
+        MediaPipe Pose Landmarker solution to extract the joint points of the body in every frames. When calling the drawing function,
+        the coordinates get saved as strings into a variable in every frame. This data is then stored in a corresponding csv file in
+        the directory given by the user.
+        
+        Parameters:
+            None
+        Returns:
+            The path to the file in string format
+        """
         self.dir_to_extract = dir_to_extract
         self.landmark_type = landmark_type
+        self.show_fps = show_fps
+        self.show_video = show_video
         self.landmark_options = self.create_landmark_options()
 
         
@@ -50,10 +63,9 @@ class CANDORPoseAnalyzer():
         More info on https://developers.google.com/mediapipe/solutions/vision/pose_landmarker
 
         Parameters:
-        landmark_type (String): which of the current 3 models do you need for pose estimation. Can be 'lite', 'full' or 'heavy'
-
+            None
         Returns:
-        The path to the file in string format
+            The path to the file in string format
         """
 
         task_file_dir = './landmark_files/'
@@ -117,8 +129,10 @@ class CANDORPoseAnalyzer():
     def create_landmark_options(self):
         """
         Define the options needed by Mediapipe Pose Landmarker.
-            Params: None
-            Returns: options (mediapipe.tasks.python.vision.PoseLandmarkerOptions): The defined options
+            Params:
+                None
+            Returns: 
+                options (mediapipe.tasks.python.vision.PoseLandmarkerOptions): The defined options used to create the Landmarker instance
         """
         landmark_path = self.check_pose_landmark_file()
         base_options = python.BaseOptions(model_asset_path=landmark_path, delegate="GPU")
@@ -144,55 +158,59 @@ class CANDORPoseAnalyzer():
         """
         frame = 0
 
-        # Skip if video file was already analyzed
-        splitted_path = os.path.split(video_path)
-        chars_to_cut_off = len(splitted_path[len(splitted_path)-1])
-        completed_path = video_path[:-chars_to_cut_off]
-        completed_file_name = completed_path + "pose_extraction_of_" + f"{splitted_path[len(splitted_path)-1]}".rstrip(".mp4") + "_completed"
-        if os.path.isfile(completed_file_name):
-            return
         output_window = None    # Used as screen variable to draw landmarks on
         last_timestamp_ms = 0   # needed by Mediapipe when in "LIVE_STREAM"-RunningMode        
         analyzed_results_person = "frame,person,x,y,z,visibility,emotion\n"  # used to store pose data of the person with the id 0 in a conversation        
         person_id = os.path.split(video_path)[1][:-4]
         feature_list = self.load_feature_file(video_path, person_id)
         
+        # if new video has begun and a pose csv file already exists, this means that an extraction run before was aborted and 
+        # the video wasn't fully analyzed (If it was, then it wouln't be on the list, bc it would be saved in the "extracted_videos.txt" 
+        # file in the user-defined pose extraction directory). Writing new values into the old file causes errors, thus delete the old file.
+        csv_file_name = self.dir_to_extract + "/" + person_id + "_posedata.csv"
+        if os.path.isfile(csv_file_name):
+            os.remove(csv_file_name)
+
         print(f"Starting analysis of video {video_path}")
         with vision.PoseLandmarker.create_from_options(self.landmark_options) as landmarker:        
             cap = cv2.VideoCapture(video_path)
             while cap.isOpened():
                 success, image = cap.read()
-                start_time = time.time()
+                if self.show_fps:
+                    start_time = time.time()
                 if not success:
-                    
-                    # create a "completed" file for pausing analysis of CANDOR dataset when movie file is over.    
                     if frame > 0:
                         analyzed_results_person = self.write_pose_to_csv(person_id, analyzed_results_person)
-                        file = open(completed_file_name, 'w', newline='')
-                        file.close()
-                    break
-                
-                # only search for poses when an emotion probability is defined in 'audio_video_features.csv'
-                # file for this frame
+                        already_extracted_file = f"{self.dir_to_extract}/extracted_videos.txt"
+                        if not os.path.isfile(already_extracted_file):
+                            file = open(already_extracted_file, 'w')
+                            file.write(f"{person_id}.mp4")
+                            file.close
+                        else:
+                            file = open(already_extracted_file, 'a')
+                            file.write(f"\n{person_id}.mp4")
+                            file.close
+                    break   
+                # only search for poses when an emotion probability is defined in 'audio_video_features.csv' file for this frame
                 if not feature_list[frame] == "":
                     mp_image = mp.Image(
                         image_format=mp.ImageFormat.SRGB,
                         data=image)
                     self.timestamp_ms = int(cv2.getTickCount() / cv2.getTickFrequency() * 1000)
                     result = landmarker.detect_for_video(mp_image, self.timestamp_ms)
-                    if len(result.pose_landmarks) == 0:
-                        print("No pose landmarkers found in this frame")
                     analyzed_results_person, output_window = self.draw_landmarks(frame, image, feature_list, analyzed_results_person, result)
-                    if output_window is not None:
+                    if self.show_video and output_window is not None:
                         cv2.imshow("MediaPipe Pose Landmark", output_window)
                 else:                
-                    cv2.imshow("MediaPipe Pose Landmark", image)
+                    if self.show_video:
+                        cv2.imshow("MediaPipe Pose Landmark", image)
                 if cv2.waitKey(1) & 0xFF == ord('q'):
                     return -1
                 if frame%200 == 0:
                     analyzed_results_person = self.write_pose_to_csv(person_id, analyzed_results_person)
-                end_time = time.time()
-                print(f"FPS of video: {1.0 / (end_time-start_time)}")
+                if self.show_fps:
+                    end_time = time.time()
+                    print(f"FPS of video {person_id}.mp4: {1.0 / (end_time-start_time)}")
                 frame += 1
 
     def write_pose_to_csv(self, person_id, csv_results):
@@ -206,12 +224,9 @@ class CANDORPoseAnalyzer():
             person_id (String): The id of the person in the video
             csv_results (String): The pose coordinates for every frame that are written to the csv file
         Returns:
-            None
+            csv_results (String): The variable that holds the pose coordinates, now empty and ready to fill with coordinates again
         """
-        if not os.path.exists(self.dir_to_extract):
-            os.makedirs(self.dir_to_extract)
         file_name = self.dir_to_extract + "/" + person_id + "_posedata.csv"
-
         # actually write to the csv file
         if os.path.isfile(file_name):
             file = open(file_name, 'a', newline='')
@@ -243,23 +258,24 @@ class CANDORPoseAnalyzer():
         """
         pose_landmarks_list = results.pose_landmarks
         annotated_image = np.copy(image)
-        # draw poses on opencv window.
         for idx in range(len(pose_landmarks_list)):
             pose_landmarks = pose_landmarks_list[idx]
             # this is used to save found points for csv files
             for landmark in pose_landmarks:
                 new_row = f"{frame},{idx},{landmark.x},{landmark.y},{landmark.z},{round(landmark.visibility,2)}, {feature_list[frame][1]}"
                 csv_results += "\n" + new_row    
-            pose_landmarks_proto = landmark_pb2.NormalizedLandmarkList()
-            pose_landmarks_proto.landmark.extend([
-                landmark_pb2.NormalizedLandmark(
-                    x=landmark.x,
-                    y=landmark.y,
-                    z=landmark.z) for landmark in pose_landmarks
-            ])
-            mp.solutions.drawing_utils.draw_landmarks(
-                annotated_image,
-                pose_landmarks_proto,
-                mp.solutions.pose.POSE_CONNECTIONS,
-                mp.solutions.drawing_styles.get_default_pose_landmarks_style())    	    
+            # draw poses on opencv window.
+            if self.show_video:
+                pose_landmarks_proto = landmark_pb2.NormalizedLandmarkList()
+                pose_landmarks_proto.landmark.extend([
+                    landmark_pb2.NormalizedLandmark(
+                        x=landmark.x,
+                        y=landmark.y,
+                        z=landmark.z) for landmark in pose_landmarks
+                ])
+                mp.solutions.drawing_utils.draw_landmarks(
+                    annotated_image,
+                    pose_landmarks_proto,
+                    mp.solutions.pose.POSE_CONNECTIONS,
+                    mp.solutions.drawing_styles.get_default_pose_landmarks_style())    	    
         return csv_results, annotated_image
--- run_pose_extraction.py
+++ run_pose_extraction.py
@@ -14,49 +14,86 @@ USE_LABAN_FEATURES = False
 CANDOR_DIR = os.environ["CANDOR_DIR"]
 os.chdir(os.path.dirname(os.path.abspath(__file__)))
 
-def extract_candor(landmark_type, dir_to_extract, max_workers):
+def extract_candor(extract_poses, landmark_type, pose_dir_to_extract, feature_dir_to_extract, max_workers, show_fps, show_video):
     """
     This function tests CANDOR dataset. It searches the video files of every participiant (not the combined video, due to computation speed) and
     uses multiprocessing to analyze each of the videos. In the end, a csv file for every video exists, with the found pose coordinates
     in every frame. 
     
     Params:
+        extract_poses (Boolean): A variable to define if the poses have to be extracted out of the video
         landmark_type (String): The MediaPipe Pose Landmarker model which is used for pose estimation
-        dir_to_extract (String): The directory where the csv files containing the pose coordinates are saved
+        pose_dir_to_extract (String): The directory where the csv files containing the pose coordinates are saved
+        feature_dir_to_extract (String): The directory where the csv file containing the features calculated from the pose coordinates is saved
         max_workers (int): The amount of parallel processes started to extract the videos
+        show_fps (Boolean): A variable to define if the current extraction speed is printed to the command line (in Frames per second)
+        show_video (Boolean): A variable to define if the candor videos are opened and poses are drawn to them.
     Returns:
         None
     """
-    
-    # get a list of all videos that still have to be analyzed
-    print("Gathering information about the video files in CANDOR dataset. Please wait...")
-    videolist = get_candor_movie_files(CANDOR_DIR)
-    
-    # for testing purposes use this line instead of the former line (on windows, on linux change path accordingly):
-    #videolist = [f"{CANDOR_DIR}/processed_dataset/processed/fffda3e6-7d99-4db8-aa12-16e99fa454c2/processed/5d5162f1b50a1000169da137.mp4", f"{CANDOR_DIR}/processed_dataset/processed/fffda3e6-7d99-4db8-aa12-16e99fa454c2/processed/5977e3867412f8000194e1fe.mp4"]
-    # start analyzing videos on video_list
-    videos_to_analyze = len(videolist)
-    print(f"{videos_to_analyze} videos still have to be analyzed. Working...")
-    analyzed_videos = 0
-    candorPoseAnalyzer = CANDORPoseAnalyzer(landmark_type, dir_to_extract)
-    with concurrent.futures.ProcessPoolExecutor(max_workers=max_workers) as executor:
-        for result in executor.map(candorPoseAnalyzer.analyze_video, videolist):
-            if result != -1:
-                analyzed_videos += 1
-                print(f"{analyzed_videos} of {videos_to_analyze} videos have been analyzed.")
-    print("All pose coordinates have been extracted from CANDOR dataset.")
-    print("Extracting the Laban features and components from found pose data. Please Wait...")
 
-    extract_all_csv_values(USE_LABAN_FEATURES, dir_to_extract)
+ 
+    if extract_poses:
+        if not os.path.exists(pose_dir_to_extract):
+            os.makedirs(pose_dir_to_extract)
+        already_extracted_file = f"{pose_dir_to_extract}/extracted_videos.txt"
+        if os.path.isfile(already_extracted_file):
+            file = open(already_extracted_file, 'r')
+            already_extracted_list = file.read().splitlines()
+            file.close
+        else:
+            already_extracted_list = []
+        # get a list of all videos that still have to be analyzed
+        print("Gathering information about the video files in CANDOR dataset. Please wait...")
+        videolist = get_candor_movie_files(CANDOR_DIR)
+        # for testing purposes use this line instead of the former line (on windows, on linux change path accordingly):
+        #videolist = [f"{CANDOR_DIR}/processed_dataset/processed/fffda3e6-7d99-4db8-aa12-16e99fa454c2/processed/5d5162f1b50a1000169da137.mp4", f"{CANDOR_DIR}/processed_dataset/processed/fffda3e6-7d99-4db8-aa12-16e99fa454c2/processed/5977e3867412f8000194e1fe.mp4"]
+        clean_video_list = []
+        for video in videolist:
+            if os.path.split(video)[1] not in already_extracted_list:
+                clean_video_list.append(video)
+        
+        # start analyzing videos on video_list
+        videos_to_analyze = len(clean_video_list)
+        print(f"{videos_to_analyze} videos still have to be analyzed. Working...")
+        analyzed_videos = 0
+        candorPoseAnalyzer = CANDORPoseAnalyzer(landmark_type, pose_dir_to_extract, show_fps, show_video)
+        with concurrent.futures.ProcessPoolExecutor(max_workers=max_workers) as executor:
+            for result in executor.map(candorPoseAnalyzer.analyze_video, clean_video_list):
+                if result != -1:
+                    analyzed_videos += 1
+                    print(f"{analyzed_videos} of {videos_to_analyze} videos have been analyzed.")
+        print("All pose coordinates have been extracted from CANDOR dataset.")
+        
+    if not feature_dir_to_extract == "":
+        print("Extracting the Laban features and components from found pose data. Please Wait...")
+        extract_all_csv_values(USE_LABAN_FEATURES, pose_dir_to_extract, feature_dir_to_extract)
     
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description='This program uses MediaPipeto to extract pose coordinates out of the CANDOR dataset.')
-    parser.add_argument('-d','--extraction_dir', help='The directory where to put the csv files that contain the extracted poses', required=True)
+    parser.add_argument('-p','--extract_poses', help='Set if you want to extract poses out of the CANDOR Dataset. Default is False', action='store_true', required=False)
+    parser.add_argument('-pdir','--pose_dir', help='MANDATORY. The directory where the csv files are located, that contain the extracted poses. If extract_poses parameter is set, pose files are extracted to this directory', required=True)
+    parser.add_argument('-fdir','--feature_dir', help='The directory where to put the csv file that contains the extracted features. If not specified, features wont be extracted', required=False)
     parser.add_argument('-m','--model', help='The Pose Landmarker Model to use with MediaPipe. Possible: lite, full and heavy. Default is heavy', required=False)
     parser.add_argument('-w','--workers', help='The amount of parallel processes that are used to extract the dataset. Default is 1', required=False)
+    parser.add_argument('-fps','--show_fps', help='Show extraction speed while extracting the poses. Default is Off',action='store_true', required=False)
+    parser.add_argument('-v','--show_video', help='Show videos while extracting the poses. Disabling may slightly increase the pose extraction speed. Default is Off',action='store_true', required=False)
     parser.parse_args()
-    args = vars(parser.parse_args())    
-    dir_to_extract = args['extraction_dir']
+    args = vars(parser.parse_args()) 
+    
+    if args['extract_poses'] is False:
+        extract_poses = False
+        print("User chose to not extract Poses out of the CANDOR video files")
+    else:
+        extract_poses = True
+        print("User chose to extract Poses out of the CANDOR video files")
+    pose_dir_to_extract = args['pose_dir']
+    if args['feature_dir'] is None:
+        feature_dir_to_extract = ""
+        print("User chose to not extract features out of the pose csv files")
+    else:
+        feature_dir_to_extract = args['feature_dir']
+        print("User chose to extract features out of the pose csv files")
     if args['model'] == 'lite':
         landmark_type = 'lite'
     elif args['model'] == 'full':
@@ -64,12 +101,20 @@ if __name__ == "__main__":
     if args['model'] == 'heavy':
         landmark_type = 'heavy'
     else:
-        landmark_typeEL_TO_CHOOSE = 'heavy'
-    if not args['workers'] == None:
-        max_workers = int(args['workers'])
-    else:
+        landmark_type = 'heavy'
+    if args['workers'] is None:
         max_workers = 1
-
+    else:
+        max_workers = int(args['workers'])
+    if args['show_fps'] is False:
+        show_fps = False
+    else:
+        show_fps = True
+    if args['show_video'] is False:
+        show_video = False
+    else:
+        show_video = True
+    
     # Assuring if GPU is used
     print("Checking CUDA and GPU support:")
     print(f"    PyTorch version built with CUDA support: {torch.version.cuda}")
@@ -78,4 +123,4 @@ if __name__ == "__main__":
     print(f"    Tensorflow found devices: {tf.config.list_physical_devices('GPU')}")
 
     # start the actual extraction    
-    extract_candor(landmark_type, dir_to_extract, max_workers)
+    extract_candor(extract_poses, landmark_type, pose_dir_to_extract, feature_dir_to_extract, max_workers, show_fps, show_video)
